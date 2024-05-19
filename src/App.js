6abae20411765428a1a49dae1c989395b2f034a9
toitimtoe <134934500+toitimtoe@users.noreply.github.com>
2024-05-20 00:09:04 +0800

    feat: add a list of items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+const header = "Hello from the other side"
+const title = "Hello"
+const table_of_contents = [
+  {id: 10, text: "My first story"},
+  {id: 20, text: "My second story"},
+  {id: 30, text: "My third story"},
+]
+
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello from the other side</h1>
-      <p>Hello</p>
-    </div>
-  )
+      <h1>{header}</h1>
+      <p>{title}</p>
+      <ul>
+        {table_of_contents.map((content, index) => (
+          <li key={index}>{content.text}</li>
+        ))}
+      </ul>
+     </div>
+ )
 }
 
 export default App
