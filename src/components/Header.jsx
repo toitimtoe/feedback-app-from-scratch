b193d37e68c25171f6658b88cdd05a8a3eed8f67
toitimtoe <134934500+toitimtoe@users.noreply.github.com>
2024-05-20 23:23:15 +0800

    feat: use styling for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,28 @@
 import PropTypes from 'prop-types'
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor}) {
+  const headerStyles = {
+    backgroundColor: bgColor, 
+    color: textColor
+  }
   return (
-    <div className="container">
-			<h1>{text}</h1>
-    </div>
-	)
+    <header style={headerStyles}>
+      <div className="container">
+        <h1>{text}</h1>
+      </div>
+    </header>
+  )
 }
 
 Header.defaultProps = {
-	text: "Feedback UI"
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95"
 }
 
 // type checking for props
 Header.propTypes = {
-	text: PropTypes.string
+  text: PropTypes.string
 }
 
 export default Header
