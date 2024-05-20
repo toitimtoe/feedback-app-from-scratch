9f62e75a6536b5ecfc3c97248163bd356d69daa8
toitimtoe <134934500+toitimtoe@users.noreply.github.com>
2024-05-20 23:45:26 +0800

    feat: add feedback item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
       <Header />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackItem />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
