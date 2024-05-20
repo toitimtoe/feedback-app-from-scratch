16558a3bff05b139187e87ddc53e5352ed4d8d5b
toitimtoe <134934500+toitimtoe@users.noreply.github.com>
2024-05-20 22:51:33 +0800

    feat: learn map and conditionals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-const header = "Hello from the other side"
-const title = "Hello"
-const table_of_contents = [
-  {id: 10, text: "My first story"},
-  {id: 20, text: "My second story"},
-  {id: 30, text: "My third story"},
-]
-
-
 function App() {
+  const header = "Hello from the other side"
+  const title = "Hello"
+  const table_of_contents = [
+    {id: 10, text: "My first story"},
+    {id: 20, text: "My second story"},
+    {id: 30, text: "My third story"},
+  ]
+
+  const loading = false
+  const showComments = true
+
+  if (loading) return <h1>Loading...</h1>
+
+  const commentBlock = (
+    <ul>
+      {table_of_contents.map((content, index) => (
+        <li key={index}>{content.text}</li>
+      ))}
+    </ul>
+  )
+
   return (
     <div className="container">
       <h1>{header}</h1>
       <p>{title}</p>
-      <ul>
-        {table_of_contents.map((content, index) => (
-          <li key={index}>{content.text}</li>
-        ))}
-      </ul>
-     </div>
- )
+      {showComments&& commentBlock}
+    </div>
+  )
 }
 
 export default App
