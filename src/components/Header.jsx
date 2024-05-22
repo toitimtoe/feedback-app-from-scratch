c605e6c795ead695c0b79b34705f200ae8cb0c73
toitimtoe <134934500+toitimtoe@users.noreply.github.com>
2024-05-22 20:22:54 +0800

    refactor: use js default params instead defaultprops

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,26 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-function Header({ text, bgColor, textColor}) {
+function Header({
+  text = "Feedback UI",
+  bgColor = "rgba(0,0,0,0.4)",
+  textColor = "#ff6a95",
+}) {
   const headerStyles = {
-    backgroundColor: bgColor, 
-    color: textColor
-  }
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
     <header style={headerStyles}>
       <div className="container">
         <h1>{text}</h1>
       </div>
     </header>
-  )
-}
-
-Header.defaultProps = {
-  text: "Feedback UI",
-  bgColor: "rgba(0,0,0,0.4)",
-  textColor: "#ff6a95"
+  );
 }
 
 // type checking for props
 Header.propTypes = {
-  text: PropTypes.string
-}
+  text: PropTypes.string,
+};
 
-export default Header
+export default Header;
