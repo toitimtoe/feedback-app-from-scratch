5133319c09f4e4b1dc9d0bbf01f511b4fbfa4734
toitimtoe <134934500+toitimtoe@users.noreply.github.com>
2024-05-22 20:00:16 +0800

    feat: add feedback items

---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,19 @@
+let FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Excellent",
+  },
+  {
+    id: 2,
+    rating: 8,
+    text: "Ok-ish",
+  },
+  {
+    id: 3,
+    rating: 1,
+    text: "Checked in and will never come back",
+  },
+];
+
+export default FeedbackData;
